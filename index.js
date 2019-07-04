f12c35b414dae43bdc449d245b67dc89287bbf23
Mvis <mining.visualizer@gmail.com>
2019-07-03 22:09:18 -0700

    tabs

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ if (process.argv[2] == '-v' || process.argv[2] == '--version') {
 var iniFile = path.join(process.cwd(), 'config.ini');
 
 if (!fs.existsSync(iniFile)) {
-	console.log('Error : unable to locate ', iniFile);
-	process.exit();
+   console.log('Error : unable to locate ', iniFile);
+   process.exit();
 }
 
 var config = ini.parse(fs.readFileSync(iniFile, 'utf-8'));
@@ -41,25 +41,25 @@ async function init() {
    var stratumClient = new StratumClient(config);
 
    if (!rpcServer) {
-		rpcServer = new RPCServer(config);
+      rpcServer = new RPCServer(config);
    }
 
    // wait for shares from the miner and send them up to the pool. use the 
    // callback to notify the miner if share was accepted.
-	rpcServer.on('submitShare', async function(args, callback) {
-		var res = await stratumClient.submitShare(args);
-		callback(res);
-	});
+   rpcServer.on('submitShare', async function(args, callback) {
+      var res = await stratumClient.submitShare(args);
+      callback(res);
+   });
 
-	// if we get disconnected from the pool, try to reconnect
+   // if we get disconnected from the pool, try to reconnect
    stratumClient.on('disconnect', function() {
-   	rpcServer.removeAllListeners('submitShare');
+      rpcServer.removeAllListeners('submitShare');
       LogB('Connection closed. Reconnecting in 5 seconds ...');
       setTimeout(init, 5000);
 
    }).on('workPackage', function(params) {
-		// listen for work packages from the stratum pool and make them available to the rpc server
-		rpcServer.setWork(params);
+      // listen for work packages from the stratum pool and make them available to the rpc server
+      rpcServer.setWork(params);
    });
 
 }
